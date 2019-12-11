47b5872e8071502bd5ded94b70703ff28a9a63be
Christian Häußler (Windows) <chrissx@chrissx.ga>
2019-12-11 19:57:29 +0100

    Updated dyndns-script and httpi-setup.

--- home/dyndns.sh
+++ home/dyndns.sh
@@ -4,25 +4,31 @@ EMAIL="chrissx@chrissx.ga"
 
 while true ; do
         IP=$(curl http://ipinfo.io/ip)
-        [ "$LAST_IP" = "$IP" ] && sleep 20 && continue
+        [ "$LAST_IP" = "$IP" ] && sleep 60s && continue
         curl -L -H "X-Auth-Email: $EMAIL" \
                 -H "X-Auth-Key: $API_KEY" \
                 -H "Content-Type: application/json" \
-                --data '{"type":"A","name":"chrissx.ga","content":"'"$IP"'","ttl":120,"proxied":false}' \
+                --data '{"type":"A","name":"zerm.eu","content":"'"$IP"'","ttl":120,"proxied":false}' \
                 -X PUT \
-                https://api.cloudflare.com/client/v4/zones/f26da62adcbb95b16f055a2dbd9fecf4/dns_records/a53863ddd00557a12736c9d442d90db4
+                https://api.cloudflare.com/client/v4/zones/b18c50790ce69441a2da12d8a78d1d02/dns_records/ba9aed375cf831f245b06e74be22c6f9
         curl -L -H "X-Auth-Email: $EMAIL" \
                 -H "X-Auth-Key: $API_KEY" \
                 -H "Content-Type: application/json" \
-                --data '{"type":"A","name":"zm.chrissx.ga","content":"'"$IP"'","ttl":120,"proxied":false}' \
+                --data '{"type":"A","name":"zerm.link","content":"'"$IP"'","ttl":120,"proxied":false}' \
                 -X PUT \
-                https://api.cloudflare.com/client/v4/zones/f26da62adcbb95b16f055a2dbd9fecf4/dns_records/f44514322e4986d180d016ae12a1aab2
+                https://api.cloudflare.com/client/v4/zones/623a7431ffe2bcc4f9a3ba85c1eb89a9/dns_records/15274d7a7fb5d2ab58f4409116da008c
         curl -L -H "X-Auth-Email: $EMAIL" \
                 -H "X-Auth-Key: $API_KEY" \
                 -H "Content-Type: application/json" \
-                --data '{"type":"A","name":"zerm.chrissx.ga","content":"'"$IP"'","ttl":120,"proxied":false}' \
+                --data '{"type":"A","name":"chrissx.de","content":"'"$IP"'","ttl":120,"proxied":false}' \
                 -X PUT \
-                https://api.cloudflare.com/client/v4/zones/f26da62adcbb95b16f055a2dbd9fecf4/dns_records/773207045238dab489f686548de22981
+                https://api.cloudflare.com/client/v4/zones/08bd4c50de27d76b981bfe6b8c911a97/dns_records/449d24589c8ade29c9624448657566e9
+        curl -L -H "X-Auth-Email: $EMAIL" \
+                -H "X-Auth-Key: $API_KEY" \
+                -H "Content-Type: application/json" \
+                --data '{"type":"A","name":"chrissx.eu","content":"'"$IP"'","ttl":120,"proxied":false}' \
+                -X PUT \
+                https://api.cloudflare.com/client/v4/zones/83843912397f637517f05b857b05c2e1/dns_records/0bc21fde502de4b36e15f07eb053bbd4
         LAST_IP="$IP"
-        sleep 120
+        sleep 240s
 done
