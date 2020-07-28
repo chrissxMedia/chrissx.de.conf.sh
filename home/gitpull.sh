7d9e79fed67f520070d476ec33890a80cdc131e3
Chris <chrissx@chrissx.de>
2020-07-28 23:56:53 +0200

    improved cipher suite and made gitpull.sh fine

--- home/gitpull.sh
+++ home/gitpull.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-while true
-do
+while true ; do
         cd /var/www/html
         git pull
         cd /var/gopher
         git pull
         cd /var/www/zerm
         git pull
-        sleep 60
+        cd /var/www/apt
+        git pull
+        cd /var/www/ampless
+        git pull
+        sleep 300
 done
