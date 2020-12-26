c05382958b90fa6a3b571e20f3be9cb32700e00f
chrissx <chrissx@chrissx.de>
2020-12-26 17:43:55 +0100

    why is this missing everywhere!?!??!

--- install_sources.sh
+++ install_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo curl -fsSLo /etc/apt/sources.list https://github.com/chrissxYT/chrissx.ga.conf.sh/raw/master/rpi.buster.sources.list
 sudo rm -rf /etc/apt/sources.list.d
-sudo apt update
-sudo apt upgrade
-sudo apt install vim tmux
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y vim tmux
