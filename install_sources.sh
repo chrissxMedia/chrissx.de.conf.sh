2cadef09e7895176ad68afa71eee08d5fbeaf474
chrissx <chrissx@chrissx.de>
2021-02-15 00:12:19 +0100

    removed servers from the list which practically dont even exist anymore

--- install_sources.sh
+++ install_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo curl -fsSLo /etc/apt/sources.list https://github.com/chrissxYT/chrissx.ga.conf.sh/raw/master/rpi.buster.sources.list
+sudo curl -fsSLo /etc/apt/sources.list https://github.com/chrissxYT/chrissx.de.conf.sh/raw/master/rpi.buster.sources.list
 sudo rm -rf /etc/apt/sources.list.d
 sudo apt update -y
 sudo apt upgrade -y
