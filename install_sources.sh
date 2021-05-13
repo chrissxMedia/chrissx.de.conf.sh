040d3685d1b1ee4af4b76efb716bd6d250a60597
chris <chrissx@chrissx.de>
2021-05-13 18:42:34 +0200

    we dont use the cache a lot anymore

--- install_sources.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo curl -fsSLo /etc/apt/sources.list https://github.com/chrissxYT/chrissx.de.conf.sh/raw/master/rpi.buster.sources.list
-sudo rm -rf /etc/apt/sources.list.d
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y vim tmux
