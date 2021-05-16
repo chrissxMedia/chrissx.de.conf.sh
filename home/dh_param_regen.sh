dd4f645a9fec4f3b5943404bd493ad418a8cc7a6
chris <chrissx@chrissx.de>
2021-05-16 16:00:04 +0200

    got rid of scripts nobody wanted anyways

--- home/dh_param_regen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-openssl dhparam -out tmp.pem 4096
-sudo mv -f tmp.pem /etc/ssl/private/dhparams.pem
--- home/temp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-watch "echo $(/opt/vc/bin/vcgencmd measure_temp | awk -F = '{print $2}' | awk -F \' '{print $1}')°C"
