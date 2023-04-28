821c833b0bd7ce46389c6559beb9283de6d3cf37
pixel <pixel@chrissx.de>
2023-04-28 07:43:38 +0200

    ali: crash when a daemon crashes

--- ali/start.sh
+++ ali/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -uxe
 
 service postfix start
 service dovecot start
@@ -6,4 +7,7 @@ service opendkim start
 
 while true ; do
         sleep 10
+        service postfix status
+        service dovecot status
+        service opendkim status
 done
